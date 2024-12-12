1c0320de4b110f5af0c43999dcdf06fd726da249
nnksknkngs <koskenkangas.anna@gmail.com>
2024-12-12 15:08:26 +0200

    Harjoitus 6b: Qt-konsolisovellus

---
+++ H6b/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include "exampleclass.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    ExampleClass example;
+    example.startToWait();
+
+    return app.exec();
+}
